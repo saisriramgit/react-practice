9727367a83003f215cbe730873a14e46c7b89766
Sai Sriram Gundepalli <saisriramgundepalli@Sais-Air.home>
2024-09-01 18:38:30 -0500

    practice router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
 import Search from './components/Search';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import Home from './components/pages/Home';
+import About from './components/pages/About';
 
 function App() {
   return (
     <div className="App">
-      <Search />
+      {/* <Search /> */}
+      <Router>
+        <div>
+          <nav>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+          </nav>
+
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/about' element={<About />} />
+          </Routes>
+        </div>
+      </Router>
+
     </div>
   );
 }
